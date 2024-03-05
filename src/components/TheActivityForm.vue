66b5c7607d5706d25aebdb09e9f5512d84d3ad53
Марек Костеневич <kostenevich@bruenok.ru>
2024-03-05 23:47:53 +0300

    вынес добавление активностей в отдельный компонент

---
+++ src/components/TheActivityForm.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+import BaseButton from './BaseButton.vue';
+import { PlusIcon } from '@heroicons/vue/24/outline/index.js';
+import { isActivityValid } from '../validators.js';
+
+
+let activity = ''
+
+function submit() {
+    emit('submit', activity)
+}
+
+
+const emit = defineEmits({
+    submit: isActivityValid
+})
+
+
+</script>
+
+<template>
+    <form
+        class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4"
+        @submit.prevent="submit"
+    >
+        <input type="text"
+               :value="activity"
+               class="w-full rounded border px-4 text-xl"
+               @input="activity = $event.target.value"
+               placeholder="Activity Name">
+        <BaseButton>
+            <PlusIcon class="h-8"></PlusIcon>
+        </BaseButton>
+    </form>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/pages/TheActivities.vue
+++ src/components/pages/TheActivities.vue
@@ -2,8 +2,8 @@
 
 import ActivityItem from './ActivityItem.vue';
 import { isActivityValid, ValidateActivities } from '../../validators.js';
-import BaseButton from '../BaseButton.vue';
-import { PlusIcon } from '@heroicons/vue/24/outline';
+
+import TheActivityForm from '../TheActivityForm.vue';
 
 const props = defineProps({
     activities: {
@@ -16,9 +16,8 @@ const props = defineProps({
 const emit = defineEmits({
     deleteActivity: isActivityValid,
     createActivity: isActivityValid
-});
 
-let newActivity = 'new';
+});
 
 </script>
 
@@ -28,19 +27,7 @@ let newActivity = 'new';
             <ActivityItem v-for="activity in activities" :key="activity" :activity="activity"
                           @delete="emit('deleteActivity', activity)" />
         </ul>
-        <form
-            class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4"
-            @submit.prevent="emit('createActivity', newActivity)"
-        >
-            <input type="text"
-                   :value="newActivity"
-                   class="w-full rounded border px-4 text-xl"
-                   @input="newActivity = $event.target.value"
-                   placeholder="Activity Name">
-            <BaseButton>
-                <PlusIcon class="h-8"></PlusIcon>
-            </BaseButton>
-        </form>
+        <TheActivityForm @submit="emit('createActivity', $event)"/>
     </div>
 </template>
 
