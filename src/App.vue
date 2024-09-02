62c0079d16dbb307d3eab0aaaef995321d6d47da
Марек Костеневич <kostenevich2004@gmail.com>
2024-09-03 00:08:41 +0300

    #46 - Выбор времени для активностей

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,73 @@
 <script setup>
-import { ref, computed } from 'vue';
-import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants';
+import { ref, computed } from 'vue'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants'
 import {
-	normalizePageHash,
-	generateTimelineItems,
-	generateActivities,
-	generateActivitySelectOptions,
-} from './functions';
-import TheHeader from './components/TheHeader.vue';
-import TheNav from './components/TheNav.vue';
-import TheTimeline from './pages/TheTimeline.vue';
-import TheActivities from './pages/TheActivities.vue';
-import TheProgress from './pages/TheProgress.vue';
+  normalizePageHash,
+  generateTimelineItems,
+  generateActivities,
+  generateActivitySelectOptions
+} from './functions'
+import TheHeader from './components/TheHeader.vue'
+import TheNav from './components/TheNav.vue'
+import TheTimeline from './pages/TheTimeline.vue'
+import TheActivities from './pages/TheActivities.vue'
+import TheProgress from './pages/TheProgress.vue'
 
-const currentPage = ref(normalizePageHash());
+const currentPage = ref(normalizePageHash())
 
-const timelineItems = ref(generateTimelineItems());
+const timelineItems = ref(generateTimelineItems())
 
-const activities = ref(generateActivities());
+const activities = ref(generateActivities())
 
-const activitySelectOptions = computed(() =>
-	generateActivitySelectOptions(activities.value),
-);
+const activitySelectOptions = computed(() => generateActivitySelectOptions(activities.value))
 
 function goTo(page) {
-	currentPage.value = page;
+  currentPage.value = page
 }
 
 function createActivity(activity) {
-	activities.value.push(activity);
+  activities.value.push(activity)
 }
 
 function deleteActivity(activity) {
-	timelineItems.value.forEach((timelineItem) => {
-		if (timelineItem.activityId === activity.id) {
-			timelineItem.activityId = null;
-		}
-	});
+  timelineItems.value.forEach((timelineItem) => {
+    if (timelineItem.activityId === activity.id) {
+      timelineItem.activityId = null
+    }
+  })
 
-	activities.value.splice(activities.value.indexOf(activity), 1);
+  activities.value.splice(activities.value.indexOf(activity), 1)
 }
 
 function setTimelineItemActivity({ timelineItem, activity }) {
-	timelineItem.activityId = activity?.id || null;
+  timelineItem.activityId = activity?.id || null
 }
 
 function setActivitySecondsToComplete(activity, secondsToComplete) {
-	activity.secondsToComplete = secondsToComplete;
+  activity.secondsToComplete = secondsToComplete
 }
 </script>
 
 <template>
-	<TheHeader @navigate="goTo($event)" />
+  <TheHeader @navigate="goTo($event)" />
 
-	<main class="flex flex-grow flex-col">
-		<TheTimeline
-			v-show="currentPage === PAGE_TIMELINE"
-			:timeline-items="timelineItems"
-			:activities="activities"
-			:activity-select-options="activitySelectOptions"
-			@set-timeline-item-activity="setTimelineItemActivity" />
-		<TheActivities
-			v-show="currentPage === PAGE_ACTIVITIES"
-			:activities="activities"
-			@create-activity="createActivity"
-			@delete-activity="deleteActivity"
-			@set-activity-seconds-to-complete="setActivitySecondsToComplete" />
-		<TheProgress v-show="currentPage === PAGE_PROGRESS" />
-	</main>
+  <main class="flex flex-grow flex-col">
+    <TheTimeline
+      v-show="currentPage === PAGE_TIMELINE"
+      :timeline-items="timelineItems"
+      :activities="activities"
+      :activity-select-options="activitySelectOptions"
+      @set-timeline-item-activity="setTimelineItemActivity"
+    />
+    <TheActivities
+      v-show="currentPage === PAGE_ACTIVITIES"
+      :activities="activities"
+      @create-activity="createActivity"
+      @delete-activity="deleteActivity"
+      @set-activity-seconds-to-complete="setActivitySecondsToComplete"
+    />
+    <TheProgress v-show="currentPage === PAGE_PROGRESS" />
+  </main>
 
-	<TheNav :current-page="currentPage" @navigate="goTo($event)" />
+  <TheNav :current-page="currentPage" @navigate="goTo($event)" />
 </template>
